20b33f8864ebe70562faa28a34c9f1a9c23c9605
RenatoFernandoSilvaGoncalves <renatogoncalves@unoeste.edu.br>
2024-09-26 18:41:41 -0300

    acrescentando "descurtidas" nos dados.

--- src/dados/candidatos.js
+++ src/dados/candidatos.js
@@ -7,6 +7,7 @@ export const listaCandidatos = [
         "avatar": "https://i.imgur.com/LDOO4Qs.jpg",
         "propostas":["laudantium enim quasi est quidem magnam voluptate ipsam eos","tempora quo necessitatibus","dolor quam autem quasi","reiciendis et nam sapiente accusantium"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -16,6 +17,7 @@ export const listaCandidatos = [
         "avatar": "https://i.imgur.com/DTfowdu.jpg",
         "propostas":["est natus enim nihil est dolore omnis voluptatem numquam","et omnis occaecati quod ullam at","voluptatem error expedita pariatur","nihil sint nostrum voluptatem reiciendis et"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -25,6 +27,7 @@ export const listaCandidatos = [
         "avatar": "https://i.imgur.com/yhW6Yw1.jpg",
         "propostas":["non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -34,6 +37,7 @@ export const listaCandidatos = [
         "avatar": "https://conteudo.imguol.com.br/c/entretenimento/5e/2017/12/29/1---liza-soberano-atriz-e-modelo-filipino-americana-1514568960645_v2_600x1.jpg.webp",
         "propostas":["doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -43,6 +47,7 @@ export const listaCandidatos = [
         "avatar": "https://conteudo.imguol.com.br/c/entretenimento/05/2017/12/29/5---nana-cantora-sul-coreana-1514568957296_v2_360x1.jpg.webp",
         "propostas":["maiores sed dolores similique labore et inventore et\nquasi temporibus esse sunt id et\neos voluptatem aliquam\naliquid ratione corporis molestiae mollitia quia et magnam dolor"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -52,6 +57,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/06/andrea-iorio-palestrante-dmtpalestras.jpg",
         "propostas":["ut voluptatem corrupti velit\nad voluptatem maiores\net nisi velit vero accusamus maiores\nvoluptates quia aliquid ullam eaque"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -61,6 +67,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/07/gustavo-ziller-palestrante-dmtpalestras.jpg",
         "propostas":["sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et ab ut omnis\net occaecati aspernatur odit sit rem expedita\nquas enim ipsam minus"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -70,6 +77,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2023/04/kelly-rodrigues-palestrante-dmtpalestras.jpg",
         "propostas":["voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et non\naut quia necessitatibus\nnostrum quaerat nulla et accusamus nisi facilis"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -79,6 +87,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/08/paulo-vieira-de-campos-palestrante-dmtpalestras.jpg",
         "propostas":["ut dolorum nostrum id quia aut est\nfuga est inventore vel eligendi explicabo quis consectetur\naut occaecati repellat id natus quo est\nut blanditiis quia ut vel ut maiores ea"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[],
     },
     {
@@ -88,6 +97,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/08/steven-dubner-palestrante-dmtpalestras.jpg",
         "propostas":["expedita maiores dignissimos facilis\nipsum est rem est fugit velit sequi\neum odio dolores dolor totam\noccaecati ratione eius rem velit"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -97,6 +107,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/08/tite-palestrante-dmtpalestras.jpg",
         "propostas":["quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]
     },
     {
@@ -106,6 +117,7 @@ export const listaCandidatos = [
         "avatar": "https://www.dmtpalestras.com.br/wp-content/uploads/2020/08/marcelo-torres-palestrante-dmtpalestras.jpg",
         "propostas":["fuga eos qui dolor rerum\ninventore corporis exercitationem\ncorporis cupiditate et deserunt recusandae est sed quis culpa\neum maiores corporis et"],
         "curtidas":0,
+        "descurtidas":0,
         "questionamentos":[]    
     }
 ];
